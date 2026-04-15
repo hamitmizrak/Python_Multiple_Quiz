725220fb4a6e7cfe957c1c7e7a8c8f23de574bbd
HamitMizrak <hamitmizrak@gmail.com>
2026-04-15 15:33:50 +0300

    ensure_results_dir() load_questions()

--- Quiz_Sample.py
+++ Quiz_Sample.py
@@ -3,19 +3,22 @@ import html
 import random
 from datetime import datetime
 from pathlib import Path
+from turtledemo.penrose import start
 
 BASE_DIR = Path(__file__).resolve().parent
 # CSV_FILE = Path(__file__).resolve().parent / "./questions.csv"
 CSV_FILE = BASE_DIR / "./questions.csv"
 RESULT_DIR = BASE_DIR / "results"
 
+#
 def ensure_results_dir():
     RESULT_DIR.mkdir(parents=True, exist_ok=True)
 
+#
 def load_questions(csv_file):
     questions =[]
 
-    if not csv_file_exists():
+    if not csv_file.exists():
         print(f"Hata CSV dosyası bulunamadı -> {csv_file}")
         return questions
 
@@ -39,5 +42,33 @@ def load_questions(csv_file):
         if missing:
             print("Hata CSV içinde eksik kolonlar var:", ", ".join(sorted(missing)))
             return questions
+
+        for now_number, row in enumerate(reader, start=2):
+            answer = str(row["answer"]).strip().upper()
+
+            if answer not in {"A","B","C","D"}:
+                print(f"Uyarı: {row_number}. satırındaki cevap geçersiz olduğu içi soru atlandı ")
+                continue
+
+            question_text =str(row["question"]).strip()
+            options ={
+                "A" : str(row["option_a"]).strip(),
+                "B" : str(row["option_b"]).strip(),
+                "C" : str(row["option_c"]).strip(),
+                "D" : str(row["option_d"]).strip(),
+            }
+
+            if not question_text:
+                print(f"Uyarı: {row_number}. satırındaki soru boş olduğu için atlandı")
+                continue
+            questions.append(
+                {
+                    "question": question_text,
+                    "options": options,
+                    "answer": answer,
+                }
+            )
+
+        return questions
 
 
